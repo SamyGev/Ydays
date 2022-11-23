c61d32797ff862cc319e18741baf6a3de5f80f22
SamyGev <samy.gevers@ynov.com>
2022-11-23 11:14:52 +0100

    second commit

---
+++ src/views/test.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Bonjour toto</h1>
+        <input type="text" v-model="msg">
+        <div v-for="lampe in lampes" :key="lampe.lampe">
+            <h1>{{lampe.lampe}}</h1>
+            <h2>{{lampe.prix}}</h2>
+            <span>{{lampe.id}}</span>
+        </div>
+        
+
+        <titi :msg="msg"></titi>
+    </div>
+
+
+</template>
+
+<script>
+import titi from '../components/HelloWorld.vue';
+import lampe from '../../lampes.json'
+export default { 
+    title: 'coucou',
+    components: {titi},
+    data(){
+        return {
+            msg:"",
+            lampes:lampe
+        }
+}
+}
+</script>
+
+<style>
+
+</style>
